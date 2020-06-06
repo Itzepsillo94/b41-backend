cbba2b13d18f792c149c19690c28cbc820e78dda
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-05 19:46:07 -0500

    Se agrega la configuracion del  api

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 require('./mongoClient');
+const api = require('./api');
+
+// Si no existe la variable de entorno PORT utiliza el puerto 3000
+const PORT = process.env.PORT || 3000;
+
+api.listen(PORT, () => console.log(`Server listening on ${PORT}`));

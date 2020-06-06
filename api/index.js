80d852473658dbb68a29844f85af3cb75cda99b4
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-05 20:19:20 -0500

    Agregar endpoint para crear usuarios

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Users } = require('../models');
 
 const api = express();
 
@@ -6,5 +7,17 @@ api.use(express.urlencoded({ extended: true }));
 api.use(express.json({ extended: true }));
 
 api.get('/', (req, res) => res.json({ message: 'Hello Dev.F' }));
+
+// Se define que todas los endpoint inicien con api
+// api.use('/api');
+
+// Users
+api.post('/api/v1/users', (req, res) => {
+  const newUser = new Users(req.body);
+
+  newUser.save()
+    .then((user) => res.status(201).json(user))
+    .catch((err) => res.status(400).json(err));
+});
 
 module.exports = api;
---
+++ models/Users.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+// Se agregara trim para validar que no exista espacios en blanco
+const UsersSchema = mongoose.Schema({
+  first_name: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  last_name: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+});
+
+const Users = mongoose.model('Users', UsersSchema);
+
+module.exports = Users;

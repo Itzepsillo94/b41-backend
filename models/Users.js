46606687a479d67daa1685c7356e452632fe3d07
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-08 20:08:32 -0500

    Instalar bcrypt y agregar encriptado de contrasenas

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,10 @@
+/* eslint-disable no-shadow */
+/* eslint-disable consistent-return */
+/* eslint-disable func-names */
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_WORK_FACTOR = 10;
 
 // Se agregara trim para validar que no exista espacios en blanco
 const UsersSchema = mongoose.Schema({
@@ -26,6 +32,27 @@ const UsersSchema = mongoose.Schema({
     type: Boolean,
     default: true,
   },
+});
+
+UsersSchema.pre('save', function (next) {
+  const user = this;
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err);
+
+    // hash the password along with our new salt
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      return next();
+    });
+  });
+  return false;
 });
 
 const Users = mongoose.model('Users', UsersSchema);

2be7de98d17a752db3f43e59c5e9a89421e78068
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-09 20:07:35 -0500

    Agregar endpoint para login de usuario; TODO: crear y responder con un JWT

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 const { UsersService } = require('../services');
+const { comparePassword } = require('../utils');
 
 module.exports = {
   register: (req, res) => {
@@ -9,5 +10,22 @@ module.exports = {
         res.status(201).json(user);
       })
       .catch((err) => res.status(400).json(err));
+  },
+  login: (req, res) => {
+    const { email, password } = req.body;
+    // 1) Comprobar que el correo existe
+    UsersService.findOneByEmail(email)
+      .then((user) => {
+        if (!user) res.status(404).json({ message: 'User not found' });
+        return comparePassword(password, user.password);
+      })
+    // 2) Comprobar la contrasena
+      .then((isValidPassword) => {
+        if (!isValidPassword) res.status(400).json({ message: 'Credentials Error' });
+        return res.status(200).json({ loggendIn: isValidPassword });
+      })
+      .catch((err) => res.status(400).json(err));
+    // 3) Crear token con las credenciales del usuario
+    // 4) Enviar token al cliente
   },
 };

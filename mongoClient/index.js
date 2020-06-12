6bf50819d87f57f05e37cebb320c35f7f1bbbf4f
Itzepsillo94 <kevin.itzep94@gmail.com>
2020-06-11 19:47:33 -0500

    se agrega variable de entorno para realizar un deploy en heroku

--- mongoClient/index.js
+++ mongoClient/index.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 
 // Asi accedemos a variables de entorno
 //  process.env.MONGO_URI
+const { MONGO_URI } = require('../config');
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
